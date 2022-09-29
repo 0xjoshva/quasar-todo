aa1a8238fd6d70a842c8ab403a2121f47e535147
0xjoshva <joshuasteed123@Gmail.com>
2022-09-29 15:13:54 +0200

    first commit

---
+++ src/pages/Todo.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-page class="bg-grey-3 column">
+    <q-list separator bordered>
+      <q-item class="bg-white" @click="task.done = !task.done" v-for="task in tasks" :key="task.title" tag="label" v-ripple clickable>
+        <q-item-section avatar>
+          <q-checkbox class="no-pointer-events" v-model="task.done"  color="primary"/>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{task.title}}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tasks: [
+        {
+          title: "Wake Up",
+          done: false,
+        },
+        {
+          title: "Eat Breakfast",
+          done: true,
+        },
+        {
+          title: "Take Shower",
+          done: false,
+        },
+      ],
+    };
+  },
+};
+</script>
